b06fceecffc75fe743199413ccf77355f8aa917a
EliasGit117 <95446990+EliasGit117@users.noreply.github.com>
2025-08-16 12:00:51 +0300

    Fix for safari for UnLazyImage

--- src/components/un-lazy-image-ssr.tsx
+++ src/components/un-lazy-image-ssr.tsx
@@ -1,9 +1,10 @@
 import { FC, ImgHTMLAttributes, useEffect } from 'react';
 import type { UnLazyLoadOptions } from 'unlazy';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { lazyLoad } from 'unlazy';
 import { createPngDataUri as createPngDataUriThumbhash } from '@unlazy/core/thumbhash';
 import { createPngDataUri as createPngDataUriBlurhash } from '@unlazy/core/blurhash';
+import { cn } from '@/lib/utils';
 
 interface IProps
   extends ImgHTMLAttributes<HTMLImageElement>,
@@ -25,24 +26,31 @@ const UnLazyImageSSR: FC<IProps> = (props) => {
     blurhash,
     thumbhash,
     placeholderSize,
+    className,
     loading = 'lazy',
     ...rest
   } = props;
   const target = useRef<HTMLImageElement | null>(null);
 
+
   const thumbhashSrc = thumbhash && createPngDataUriThumbhash(thumbhash);
   const blurhashSrc = blurhash && createPngDataUriBlurhash(blurhash);
   const placeholderSrc = thumbhashSrc ?? blurhashSrc;
 
-  const [realSrc, setRealSrc] = useState<string | undefined>();
 
   useEffect(() => {
-    if (!target.current) return;
+    if (!target.current)
+      return;
+
     const cleanup = lazyLoad(target.current, {
       hash: thumbhash || blurhash,
       hashType: thumbhash ? 'thumbhash' : 'blurhash',
-      placeholderSize
+      placeholderSize,
+      onImageLoad: () => {
+        target.current?.classList.remove('safari:blur-xs', 'safari:scale-105');
+      }
     });
+
     return () => cleanup();
   }, [thumbhash, blurhash, placeholderSize]);
 
@@ -50,11 +58,12 @@ const UnLazyImageSSR: FC<IProps> = (props) => {
     <img
       ref={target}
       alt={alt}
-      src={realSrc ?? placeholderSrc}
-      data-src={realSrc ? undefined : src}
-      data-srcset={realSrc ? undefined : srcSet}
+      src={placeholderSrc}
+      data-src={src}
+      data-srcset={src}
       data-sizes={autoSizes ? 'auto' : undefined}
       loading={loading}
+      className={cn('safari:blur-xs safari:scale-105', className)}
       {...rest}
     />
   );
