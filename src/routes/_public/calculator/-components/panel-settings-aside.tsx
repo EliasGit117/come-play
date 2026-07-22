043b947a1374fc86b44b8697f07c6553457f3f31
EliasGit117 <95446990+EliasGit117@users.noreply.github.com>
2026-07-22 14:29:28 +0300

    Adapted a bit UI for calculation page v2

--- src/routes/_public/calculator/-components/panel-settings-aside.tsx
+++ src/routes/_public/calculator/-components/panel-settings-aside.tsx
@@ -9,7 +9,7 @@ const PanelSettingsAside: FC = () => {
   return (
     <aside className={cn('sticky top-16 pt-14 w-82 max-h-[calc(90dvh-4rem-2rem)] hidden xl:block')}>
       <ScrollArea type="always" className='flex-1 overflow-y-auto pr-4 mr-4'>
-        <div className='flex flex-col gap-2 max-h-[calc(100dvh-15dvh-4rem-2rem-3.5rem)]'>
+        <div className='flex flex-col gap-2 max-h-[calc(100dvh-4rem-2rem-3.5rem)]'>
           <PanelSettingsFields/>
           <SendRequestDialog className="w-full mt-4"/>
         </div>
