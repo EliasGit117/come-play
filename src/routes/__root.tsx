929129d16469261da6e9c7a84ab1515f3c5c9b41
EliasGit117 <95446990+EliasGit117@users.noreply.github.com>
2026-07-23 20:15:27 +0300

    Added icons and fixed web manifest

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -36,8 +36,8 @@ export const Route = createRootRouteWithContext<IRootRouteProps>()({
     links: [
       { rel: 'stylesheet', href: appCss },
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
       { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
       { rel: 'icon', href: '/favicon.ico' }
     ]
